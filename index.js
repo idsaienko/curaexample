87e9e12774454ef9343b4a3ab4654e2cef533435
idsaienko <i.d.saienko@gmail.com>
2026-01-09 03:53:13 +0100

    added frontend url

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(
     origin: [
       "http://localhost:5173", 
       "https://curalink.care", // Deployed frontend
-      "http://curafront-production.up.railway.app"
+      "http://curafront.up.railway.app"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
